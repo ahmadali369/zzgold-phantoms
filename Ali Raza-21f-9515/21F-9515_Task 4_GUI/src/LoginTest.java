3b5821953205d8235e8ffeacebfe2412ac9a67f8
Syed Ali Raza <136948112+AliRazaSyed7806@users.noreply.github.com>
2023-11-03 23:25:48 +0500

    Update LoginTest.java

--- Ali Raza-21f-9515/21F-9515_Task 4_GUI/src/LoginTest.java
+++ Ali Raza-21f-9515/21F-9515_Task 4_GUI/src/LoginTest.java
@@ -1,4 +1,92 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
 
+    public static void main(String[] args) {
+        // Set up Selenium WebDriver (make sure you have ChromeDriver installed and its path set)
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        
+        // Scenario 1: Verify that the login page loads correctly with input fields
+        driver.get("https://example.com/login");
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        
+        // Scenario 2: Check if the "Forgot Password" link is functional
+        WebElement forgotPasswordLink = driver.findElement(By.linkText("Forgot Password"));
+        forgotPasswordLink.click();
+        // You can add assertions to verify the behavior of the "Forgot Password" functionality
+        
+        // Scenario 3: Test if the API displays an error message for empty input
+        WebElement loginButton = driver.findElement(By.id("loginButton"));
+        loginButton.click();
+        WebElement errorMessage = driver.findElement(By.id("errorMessage"));
+        // Add an assertion to check if the error message is displayed
+        
+        // Scenario 4: Ensure the API rejects login attempts with invalid characters
+        usernameInput.sendKeys("invalid@user");
+        passwordInput.sendKeys("invalid#pass");
+        loginButton.click();
+        // Add an assertion to check if the error message is displayed
+        
+        // Scenario 5: Verify that the "Login" API logs the user in with valid credentials
+        usernameInput.clear();
+        passwordInput.clear();
+        usernameInput.sendKeys("validUser");
+        passwordInput.sendKeys("validPassword");
+        loginButton.click();
+        // Add assertions to check for successful login
+        
+        // Scenario 6: Check if the "Cancel" button clears the input fields
+        driver.get("https://example.com/login");
+        usernameInput.sendKeys("someUser");
+        passwordInput.sendKeys("somePassword");
+        WebElement cancelButton = driver.findElement(By.id("cancelButton"));
+        cancelButton.click();
+        // Add assertions to check if input fields are cleared
+        
+        // Scenario 7: Test if the system provides feedback on password strength
+        WebElement newPasswordInput = driver.findElement(By.id("newPassword"));
+        newPasswordInput.sendKeys("weakPassword");
+        // Perform actions to trigger password strength feedback
+        // Add assertions for the feedback
+        
+        // Scenario 8: Ensure the system enforces a minimum password strength level
+        newPasswordInput.clear();
+        newPasswordInput.sendKeys("12345");
+        // Perform actions to trigger the password strength error
+        // Add assertions for the error message
+        
+        // Scenario 9: Test what happens when a user is inactive and tries to resume the session
+        driver.get("https://example.com/session");
+        // Implement actions and assertions for session resumption
+        
+        // Scenario 10: Confirm that all links and buttons are working
+        driver.get("https://example.com/login");
+        // Implement interactions with all links and buttons
+        // Add assertions for each interaction
+        
+        // Scenario 11: Check that the "Back" button functions as expected
+        driver.get("https://example.com/other-page");
+        WebElement backButton = driver.findElement(By.id("backButton"));
+        backButton.click();
+        // Implement assertions to check if the user is returned to the previous page
+        
+        // Scenario 12: Test the GUI's behavior when the server is down
+        // This might require additional configuration and handling of server errors
+        
+        // Scenario 13: Check if the GUI supports multiple languages
+        WebElement languageSelector = driver.findElement(By.id("languageSelector"));
+        // Implement actions to change the language setting
+        // Add assertions to check if the text labels are displayed in the selected language
+        
+        // Scenario 14: Test for concurrent user logins
+        // This scenario may involve parallel execution or multiple test threads
+        
+        // Close the WebDriver
+        driver.quit();
+    }
 }
